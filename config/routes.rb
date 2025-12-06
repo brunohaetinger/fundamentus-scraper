41199e1e8cd5ec7800d88d81b5640247e336993b
Bruno Haetinger <brunohaetinger@gmail.com>
2025-12-06 13:09:00 -0300

    Generate rails Stocks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "stocks/index"
+  get "stocks/show"
+  get "stocks/new"
+  get "stocks/create"
+  get "stocks/fetch"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
