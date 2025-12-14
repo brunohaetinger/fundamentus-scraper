cfb6292e74e639b68eb2ce76fb11186ae7382389
Bruno Haetinger <brunohaetinger@gmail.com>
2025-12-14 14:09:52 -0300

    Magic formula controller function

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -37,6 +37,33 @@ class StocksController < ApplicationController
     render :new
   end
 
+  def magic_formula
+    stocks = Stock.where.not(pl: nil).where.not(roe: nil)
+    @stocks_with_ranks = []
+    return if stocks.empty?
+
+    pl_ranks = {}
+    stocks.order(:pl).each_with_index do |stock, idx|
+      pl_ranks[stock.id] = idx + 1
+    end
+
+    roe_ranks = {}
+    stocks.order(roe: :desc).each_with_index do |stock, idx|
+      roe_ranks[stock.id] = idx + 1
+    end
+
+    @stocks_with_ranks = stocks.map do |stock|
+      stock.attributes.merge(
+        "rank_pl" => pl_ranks[stock.id],
+        "rank_roe" => roe_ranks[stock.id],
+        "magic" => pl_ranks[stock.id] + roe_ranks[stock.id]
+      )
+    end
+
+    sort_column = params[:sort] || "magic"
+    @stocks_with_ranks.sort_by! { |s| s[sort_column] || Float::INFINITY }
+  end
+
   def fetch
     @stock = Stock.find(params[:id])
     result = Fundamentus::Fetcher.fetch(@stock.ticker)
