31c9798ab918e6de48d2a63d229ae83c02ebc7c3
Bruno Haetinger <brunohaetinger@gmail.com>
2025-12-17 07:25:50 -0300

    Rename fundament scraper service

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -38,37 +38,37 @@ class StocksController < ApplicationController
   end
 
   def magic_formula
-    render layout: 'application'
+    render layout: "application"
   end
-    # stocks = Stock.where.not(pl: nil).where.not(roe: nil)
-    # @stocks_with_ranks = []
-    # return if stocks.empty?
-    #
-    # pl_ranks = {}
-    # stocks.order(:pl).each_with_index do |stock, idx|
-    #   pl_ranks[stock.id] = idx + 1
-    # end
-    #
-    # roe_ranks = {}
-    # stocks.order(roe: :desc).each_with_index do |stock, idx|
-    #   roe_ranks[stock.id] = idx + 1
-    # end
-    #
-    # @stocks_with_ranks = stocks.map do |stock|
-    #   stock.attributes.merge(
-    #     "rank_pl" => pl_ranks[stock.id],
-    #     "rank_roe" => roe_ranks[stock.id],
-    #     "magic" => pl_ranks[stock.id] + roe_ranks[stock.id]
-    #   )
-    # end
-    #
-    # sort_column = params[:sort] || "magic"
-    # @stocks_with_ranks.sort_by! { |s| s[sort_column] || Float::INFINITY }
+  # stocks = Stock.where.not(pl: nil).where.not(roe: nil)
+  # @stocks_with_ranks = []
+  # return if stocks.empty?
+  #
+  # pl_ranks = {}
+  # stocks.order(:pl).each_with_index do |stock, idx|
+  #   pl_ranks[stock.id] = idx + 1
+  # end
+  #
+  # roe_ranks = {}
+  # stocks.order(roe: :desc).each_with_index do |stock, idx|
+  #   roe_ranks[stock.id] = idx + 1
+  # end
+  #
+  # @stocks_with_ranks = stocks.map do |stock|
+  #   stock.attributes.merge(
+  #     "rank_pl" => pl_ranks[stock.id],
+  #     "rank_roe" => roe_ranks[stock.id],
+  #     "magic" => pl_ranks[stock.id] + roe_ranks[stock.id]
+  #   )
+  # end
+  #
+  # sort_column = params[:sort] || "magic"
+  # @stocks_with_ranks.sort_by! { |s| s[sort_column] || Float::INFINITY }
   def magic_formula_data
-    data = FundamentusScraper.scrape
+    data = FundamentusScraperService.scrape
     render json: data
   rescue => e
-    render json: {error: e.message }, status: :internal_server_error
+    render json: { error: e.message }, status: :internal_server_error
   end
 
   def fetch
